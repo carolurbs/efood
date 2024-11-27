c6d72d3248abc7955e1d40631d32232768d221f5
Caroline Urbainski <carolineurb.dev@outlook.com>
2024-11-27 16:23:40 -0300

    header

---
+++ src/components/header/profile/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const HeaderBar = styled.header`
+  width: 2024px;
+  height: 184px;
+`
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,11 @@
 import { createGlobalStyle } from 'styled-components'
+export const colors = {
+  salmon: '#E66767',
+  cream: '#FFF8F1',
+  white: '#fff'
+}
 const EstiloGlobal = createGlobalStyle`
+
 *{
 margin:0;
 padding:0;
@@ -8,5 +14,10 @@ font-family: Roboto, sans-serif;
 list-style: none;
 text-decoration:none;
 }
+body{
+background-color: ${colors.cream};
+color: ${colors.salmon};
+}
 `
+
 export default EstiloGlobal
