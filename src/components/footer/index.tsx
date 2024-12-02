549d8bd23fafb3f9f8f7ed0f71baf37a00f5b3ff
carolurbs <carolineurb.dev@outlook.com>
2024-12-02 14:33:24 -0300

    footer final

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,13 +1,38 @@
-import { FooterBar } from './styles'
+import * as S from './styles'
 import logo from '../../assets/logo.svg'
+import twiiter from '../../assets/twitter.svg'
+import facebook from '../../assets/facebook.svg'
+import instagram from '../../assets/instagram.svg'
+
 const Footer = () => (
-  <FooterBar>
-    <div>
+  <S.FooterBar>
+    <div className="container">
       <img src={logo} alt="Efood" />
       <nav>
-        <ul></ul>
+        <S.Links>
+          <li>
+            <a href="#">
+              <img src={instagram} alt="Instagram" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={facebook} alt="Facebook" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={twiiter} alt="Twitter" />
+            </a>
+          </li>
+        </S.Links>
       </nav>
     </div>
-  </FooterBar>
+    <S.Disclaimer>
+      A efood é uma plataforma para divulgação de estabelecimentos, a
+      responsabilidade pela entrega, qualidade dos produtos é toda do
+      estabelecimento contratado.{' '}
+    </S.Disclaimer>
+  </S.FooterBar>
 )
 export default Footer
--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -10,5 +10,22 @@ export const FooterBar = styled.footer`
   align-items: center;
   div {
     margin-top: 36px;
+    padding-right: 176px;
   }
+`
+export const Links = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 32px 20px;
+`
+
+export const Disclaimer = styled.p`
+  font-size: 10px;
+  font-weight: 400;
+  text-align: center;
+  max-width: 560px;
+  display: inline;
+  margin-top: 40px;
+  padding-right: 176px;
 `
